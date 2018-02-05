8787ffae2e2311a0a706e54a5a250905d4456dce
Anatoly Dolgov <anatoldd@list.ru>
2018-02-05 09:10:50 +0500

    fix

--- index.php
+++ index.php
@@ -130,18 +130,18 @@ $lots = [$lot1, $lot2, $lot3, $lot4, $lot5, $lot6];
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($lots as $key => $value): ?>
+            <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$value['photo']?>" width="350" height="260" alt="<?=$value['alt']?>">
+                    <img src="<?=$lot['photo']?>" width="350" height="260" alt="<?=$lot['alt']?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$value['category']?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['title']?></a></h3>
+                    <span class="lot__category"><?=$lot['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['title']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lot['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -150,25 +150,6 @@ $lots = [$lot1, $lot2, $lot3, $lot4, $lot5, $lot6];
                 </div>
             </li>
             <?php endforeach; ?>
-
-            <!-- <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-
-                        </div>
-                    </div>
-                </div>
-            </li> -->
         </ul>
     </section>
 </main>
@@ -176,29 +157,11 @@ $lots = [$lot1, $lot2, $lot3, $lot4, $lot5, $lot6];
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($categories as $key => $value): ?>
+            <?php foreach ($categories as $category): ?>
             <li class="nav__item">
-                <a href="all-lots.html"><?=$value?></a>
+                <a href="all-lots.html"><?=$category?></a>
             </li>
             <?php endforeach; ?>
-            <!-- <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li> -->
         </ul>
     </nav>
     <div class="main-footer__bottom container">
