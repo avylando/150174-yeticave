f91030392563b08ecababfcee34d931f38e73925
Anatoly Dolgov <anatoldd@list.ru>
2018-02-06 12:43:37 +0500

    fix2

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ function format_price($price) {
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=format_price($lot['price'])?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_price($lot['price'])?></span>
                         </div>
                         <div class="lot__timer timer">
 
