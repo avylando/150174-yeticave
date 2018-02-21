931b70a7c186844b07a89671cdfbaff55e8a226b
Anatoly Dolgov <anatoldd@list.ru>
2018-02-21 17:12:19 +0500

    +

--- schema.sql
+++ schema.sql
@@ -2,44 +2,47 @@ CREATE DATABASE yeticave_db;
 
 USE yeticave_db;
 
-CREATE TABLE lots (
+CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date DATE,
-  name CHAR(64),
-  category_id INT,
-  description CHAR(255),
-  photo CHAR(128),
-  start_price INT,
-  step INT,
-  expiration_date DATE,
-  author_id INT,
-  winner_id INT
+  name CHAR(64) NOT NULL,
+  mail CHAR(64) NOT NULL,
+  password CHAR(64) NOT NULL,
+  contacts CHAR(255) NOT NULL,
+  avatar CHAR(128) DEFAULT 'img/user.jpg'
 );
 
-CREATE TABLE categories (
+CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(64)
+  name CHAR(64) NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(64),
-  mail CHAR(64),
-  password CHAR(64),
-  avatar CHAR(128),
-  contacts CHAR(255),
-  created_lots_id INT,
-  bets_id INT
+  date DATETIME NOT NULL DEFAULT NOW(),
+  name CHAR(64) NOT NULL,
+  category_id INT NOT NULL,
+  message CHAR(255) NOT NULL,
+  photo CHAR(128) NOT NULL,
+  start_price INT NOT NULL,
+  step INT NOT NULL,
+  expiration_date DATETIME NOT NULL,
+  author_user_id INT NOT NULL,
+  winner_user_id INT NOT NULL,
+  CONSTRAINT FK_LotCategory FOREIGN KEY (category_id) REFERENCES category(id),
+  CONSTRAINT FK_LotAuthor FOREIGN KEY (author_user_id) REFERENCES user(id),
+  CONSTRAINT FK_LotWinner FOREIGN KEY (winner_user_id) REFERENCES user(id)
 );
 
-CREATE TABLE bets (
+CREATE TABLE bet (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date DATE,
-  sum INT,
-  user_id INT,
-  lot_id INT
+  betdate DATETIME NOT NULL DEFAULT NOW(),
+  sum INT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL,
+  CONSTRAINT FK_BetUser FOREIGN KEY (user_id) REFERENCES user(id),
+  CONSTRAINT FK_BetLot FOREIGN KEY (lot_id) REFERENCES lot(id)
 );
 
-CREATE UNIQUE INDEX mail ON users (mail);
-CREATE INDEX lot_name ON lots (name);
-CREATE INDEX lot_category ON lots (category_id);
+CREATE UNIQUE INDEX mail ON user (mail);
+CREATE INDEX lot_name ON lot (name);
+CREATE INDEX lot_category ON lot (category_id);
